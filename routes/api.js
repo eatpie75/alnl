3e53ce694cf876caa3a89bb309532a5543c9d444
eatpie75 <eatpie75@gmail.com>
2015-07-25 16:48:32 -0700

    move photo upload to api make get_gcloud util

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
+var busboy = require('connect-busboy');
 var fuzzy_selection_update = require('../utils/fuzzy_selection_update');
+var get_gcloud = require('../utils/get_gcloud');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
+var uuid = require('node-uuid');
 
 var db = require('../models');
 
@@ -49,6 +53,38 @@ router.get('/thing_suggest', function(req, res, next) {
       };
     }));
   });
+});
+
+router.use('/upload/:entry', busboy());
+router.post('/upload/:entry', function(req, res) {
+  if (!req.busboy) {
+    res.status(400).send();
+    return;
+  }
+
+  req.busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    var gcloud = get_gcloud();
+
+    var ext = path.extname(filename), name = filename.replace(ext, '');
+    var gfile = gcloud.bucket('alnl').file(name + '__' + uuid.v4() + ext);
+
+    var gf_pipe = file.pipe(gfile.createWriteStream());
+    gf_pipe.on('complete', function(metadata) {
+      gfile.makePublic();
+
+      var model = {
+        'name': filename,
+        'gid': metadata.name,
+        'EntryId': req.params.entry
+      };
+
+      db.models.Photo.create(model).then(function() {
+        res.status(201).send();
+      });
+    });
+  });
+
+  req.pipe(req.busboy);
 });
 
 module.exports = router;
