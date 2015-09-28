1b1c85097e2d607c03de217bff465282f885cb54
eatpie75 <eatpie75@gmail.com>
2015-09-28 16:48:52 -0700

    add tests for thingtags

---
+++ test/thingtag.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect;
+var markdownit = require('../lib/markdown-it/dist/markdown-it');
+var thingtag = require('../public/js/thingtag');
+
+var md = markdownit('commonmark', {'typographer': true});
+md.use(thingtag);
+
+describe('thingtag', function() {
+  it('should find thingtags', function() {
+    expect(md.render('@tag')).to.be.eql('<p><a href="/thing/tag" class="thing">tag</a></p>\n');
+    expect(md.render('@tag extra')).to.be.eql('<p><a href="/thing/tag" class="thing">tag</a> extra</p>\n');
+    expect(md.render('extra @tag extra')).to.be.eql('<p>extra <a href="/thing/tag" class="thing">tag</a> extra</p>\n');
+    expect(md.render('extra @tag\'s extra')).to.be.eql('<p>extra <a href="/thing/tag" class="thing">tag</a>\'s extra</p>\n');
+    expect(md.render('extra @tag.com extra')).to.be.eql('<p>extra <a href="/thing/tag" class="thing">tag</a>.com extra</p>\n');
+  });
+
+  it('shouldn\'t find thingtags in the middle of words', function() {
+    expect(md.render('not@matched')).to.be.eql('<p>not@matched</p>\n');
+    expect(md.render('not@matched @tag')).to.be.eql('<p>not@matched <a href="/thing/tag" class="thing">tag</a></p>\n');
+    expect(md.render('not@matched @tag extra')).to.be.eql('<p>not@matched <a href="/thing/tag" class="thing">tag</a> extra</p>\n');
+    expect(md.render('extra not@matched @tag extra')).to.be.eql('<p>extra not@matched <a href="/thing/tag" class="thing">tag</a> extra</p>\n');
+  });
+
+  it('shouldn\'t find thingtags without text', function() {
+    expect(md.render('not matched @')).to.be.eql('<p>not matched @</p>\n');
+    expect(md.render('not matched @ extra')).to.be.eql('<p>not matched @ extra</p>\n');
+    expect(md.render('not matched @\nextra')).to.be.eql('<p>not matched @\nextra</p>\n');
+    expect(md.render('@@')).to.be.eql('<p>@@</p>\n');
+  });
+
+});
---
+++ test/utils.js
@@ -0,0 +1,38 @@
+var expect = require('chai').expect;
+var markdownit = require('../lib/markdown-it/dist/markdown-it');
+var thingtag = require('../public/js/thingtag');
+var utils = require('../public/js/utils');
+
+var md = markdownit('commonmark', {'typographer': true});
+md.use(thingtag);
+
+describe('utils', function() {
+  describe('get_parsed_thingtags', function() {
+    var get_parsed_thingtags = utils.get_parsed_thingtags;
+
+    it('should return an array', function() {
+      var result;
+
+      result = get_parsed_thingtags(md);
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(0);
+
+      result = get_parsed_thingtags(md, '');
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(0);
+
+      result = get_parsed_thingtags(md, '@tag');
+      expect(result).to.be.an('array');
+      expect(result).to.have.length(1);
+    });
+
+    it('should find parsed thingtags', function() {
+      var result = get_parsed_thingtags(md, '@tag extra @tag2');
+
+      expect(result).to.have.length(2);
+      expect(result[0].content).to.be.eql('tag');
+      expect(result[1].content).to.be.eql('tag2');
+    });
+
+  });
+});
